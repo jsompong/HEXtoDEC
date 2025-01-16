b97476c2f8145dfb7cab3c0943d7af0ec769d997
Sompong JongmEEsUK <j_sompong@live.com>
2025-01-16 14:19:16 +0700

    Hex to Dec VS2022
    16 January 2025, J.Sompong

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WFAnet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HEXlistBox = new System.Windows.Forms.ListBox();
+            this.DEClistBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HEXlistBox
+            // 
+            this.HEXlistBox.FormattingEnabled = true;
+            this.HEXlistBox.Location = new System.Drawing.Point(88, 121);
+            this.HEXlistBox.Name = "HEXlistBox";
+            this.HEXlistBox.Size = new System.Drawing.Size(65, 212);
+            this.HEXlistBox.TabIndex = 0;
+            this.HEXlistBox.SelectedIndexChanged += new System.EventHandler(this.HEXlistBox_SelectedIndexChanged);
+            // 
+            // DEClistBox
+            // 
+            this.DEClistBox.FormattingEnabled = true;
+            this.DEClistBox.Location = new System.Drawing.Point(169, 121);
+            this.DEClistBox.Name = "DEClistBox";
+            this.DEClistBox.Size = new System.Drawing.Size(65, 212);
+            this.DEClistBox.TabIndex = 1;
+            this.DEClistBox.SelectedIndexChanged += new System.EventHandler(this.DEClistBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "HEX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DEC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DEClistBox);
+            this.Controls.Add(this.HEXlistBox);
+            this.Name = "Form1";
+            this.Text = "Sine Wave HEX to DEC (VS Community 2022)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox HEXlistBox;
+        private System.Windows.Forms.ListBox DEClistBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using CsvHelper;
+using System.Globalization;
+using static System.Console;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WFAnet
+   
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         //    public static string ReadAllText(string path);
+        InitializeComponent();
+            
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // var fs = new FileStream(@"D:\WFAnet\bin\Debug\sinewaveHEX.csv", FileMode.Open);
+            var fs = new FileStream(@"D:\WFAnet\bin\Debug\sinewaveHEX1.csv", FileMode.Open);
+            var xreader = new StreamReader(fs);
+            while (!xreader.EndOfStream)
+            {
+                var line = xreader.ReadLine();
+              //  WriteLine(line?.ToUpperInvariant());
+                string hexdecim = line?.ToUpperInvariant();
+             //   MessageBox.Show(hexdecim);                  // แสดง เป็น กลุ่ม
+            }
+            xreader.Close();
+
+            //  จาก data :  อ่าน ฐาน 16 เป็น ฐาน 10            // Install-Package CsvHelper
+           //  var reader = new StreamReader("sinewaveHEX.csv");
+            var reader = new StreamReader("sinewaveHEX1.csv");
+
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<dynamic>();                         // read CSV file
+                                                                             // output
+            foreach (var r in records)
+            {
+                //WriteLine($"{r.FirstName,-15}{r.LastName,-10}{r.JoinedDate,15}{r.Salary,15}{r.Active,5}");
+                //WriteLine($"{r.HexValue,-15}");
+                //string hexdecim = $"{r.HexValue,-15}";
+                //   string hexdecim = $"{r.HexCol1}{r.HexCol2}{r.HexCol3}{r.HexCol4}";
+                string hexdecim = $"{r.HexColumn}";
+                //MessageBox.Show(hexdecim);
+                HEXlistBox.Items.Add(hexdecim);
+                int decValue = int.Parse(hexdecim, System.Globalization.NumberStyles.HexNumber);
+                string decim = Convert.ToString(decValue);
+                //  MessageBox.Show(decim);
+                DEClistBox.Items.Add(decim);
+            }
+            reader.Close();
+            //try
+            //{
+            //  Block of code to try
+            //}
+            //catch (Exception e)
+            //{
+            //  Block of code to handle errors
+            //}
+
+
+            try
+            {
+                //string contents = File.ReadAllText(@"C:\temp\readme1.txt");
+                string contents = File.ReadAllText(@"D:\WFAnet\bin\Debug\sinewaveHEX.csv");
+               // Console.WriteLine(contents);
+               //  MessageBox.Show(contents);              // แสดง เป็นกลุ่ม
+            }
+         //   catch (FileNotFoundException e)
+            catch (Exception)
+            {
+                //Console.WriteLine(e.Message);
+            }
+
+        }
+
+        private void HEXlistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DEClistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
